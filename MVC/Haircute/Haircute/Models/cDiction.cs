b757733974ae6d3ec127598a23fa8f9f2ab180c8
alan825444 <alan825444@gmail.com>
2021-10-16 21:32:51 +0800

    email帳號驗證

---
+++ MVC/Haircute/Haircute/Models/cDiction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Haircute.Models
+{
+    public class cDiction
+    {
+        public static readonly string SENDGRID_KEY = "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";
+    }
+}
---
+++ MVC/Haircute/Haircute/Models/registFunction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+using Haircute.Models;
+
+namespace Haircute.Models
+{
+    public class registFunction
+    {
+        public async Task SendEmail()
+        {
+            var apiKey = cDiction.SENDGRID_KEY;
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("alan825444@gmail.com", "Example User");
+            var to = new EmailAddress("alan825444@gmail.com", "Example User");
+            var subject = "Sending with Twilio SendGrid is Fun";
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+        }
+    }
+}
