28fb893618657150f3314e047e4d0e015861b00a
alan825444 <alan825444@gmail.com>
2021-10-17 15:21:02 +0800

    test

--- MVC/Haircute/Haircute/Models/cDiction.cs
+++ MVC/Haircute/Haircute/Models/cDiction.cs
@@ -8,5 +8,7 @@ namespace Haircute.Models
     public class cDiction
     {
         public static readonly string SENDGRID_KEY = "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";
+
+        public static string A() { };
     }
 }
