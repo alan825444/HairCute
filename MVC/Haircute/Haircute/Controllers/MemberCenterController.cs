890240e4a22e1b842a54dfb8d60ca8338ecf691e
alan825444 <alan825444@gmail.com>
2021-11-02 23:54:38 +0800

    11/2後臺圖片上傳
    可以上傳儲存到伺服器，將檔名寫入資料庫及其他後續操作未完成

--- MVC/Haircute/Haircute/Controllers/MemberCenterController.cs
+++ MVC/Haircute/Haircute/Controllers/MemberCenterController.cs
@@ -15,5 +15,22 @@ namespace Haircute.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult photoupdate(string filename, HttpPostedFileBase blob)
+        {
+            string systemFileExtenstion = filename.Substring(filename.LastIndexOf('.'));
+            var newfileName180 = GenerateFileName("PhotoDesigner", systemFileExtenstion);
+            var fullpath = "~/Images/" + newfileName180;
+            blob.SaveAs(Server.MapPath(fullpath));
+            return Json(new { Message = "OK" });
+        }
+
+        public string GenerateFileName(string fileTypeName, string fileextenstion)
+        {
+            if (fileTypeName == null) throw new ArgumentNullException(nameof(fileTypeName));
+            if (fileextenstion == null) throw new ArgumentNullException(nameof(fileextenstion));
+            return $"{fileTypeName}_{DateTime.Now:yyyyMMddHHmmssfff}_{Guid.NewGuid():N}{fileextenstion}";
+        }
     }
 }
