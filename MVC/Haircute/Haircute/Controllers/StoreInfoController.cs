316fc649a598a38a419a6286721427808ffc9115
alan825444 <alan825444@gmail.com>
2021-11-09 13:15:10 +0800

    11/9修正bug

--- MVC/Haircute/Haircute/Controllers/StoreInfoController.cs
+++ MVC/Haircute/Haircute/Controllers/StoreInfoController.cs
@@ -11,23 +11,50 @@ namespace Haircute.Controllers
     {
         // GET: StoreInfo
         demodbEntities db = new demodbEntities();
-        public ActionResult Index(int? id)
+        public ActionResult Index()
         {
-            /*int SSID = Convert.ToInt32(User.Identity.Name);*/
-            int SSID = (int)id;
-            var q = db.tDesigner.Where(x => x.fid == SSID).FirstOrDefault();
-            ViewBag.Name = db.tMember.Where(x => x.fID == q.fk_Member).FirstOrDefault().fNickname;
-            if(q != null)
+            if (User.Identity.Name =="")
             {
-                
-                ViewBag.HImg = new 資料產生器Test().取得會員大頭貼(SSID);
-                ViewBag.Dphoto = new 資料產生器Test().取得設計師作品(SSID);
-                ViewBag.店鋪資訊 = new 資料產生器Test().設計師店鋪(SSID);
-                ViewBag.服務項目 = new 資料產生器Test().服務項目(SSID);
-                ViewBag.評價 = new 資料產生器Test().評價(SSID);
-                
+                int SSID = (int)TempData["ID"];
+                Session["ID"] = SSID;
+                var q = db.tDesigner.Where(x => x.fid == SSID).FirstOrDefault();
+                ViewBag.Name = db.tMember.Where(x => x.fID == q.fk_Member).FirstOrDefault().fNickname;
+                if (q != null)
+                {
+
+                    ViewBag.HImg = new 資料產生器Test().取得會員大頭貼(SSID);
+                    ViewBag.Dphoto = new 資料產生器Test().取得設計師作品(SSID);
+                    ViewBag.店鋪資訊 = new 資料產生器Test().設計師店鋪(SSID);
+                    ViewBag.服務項目 = new 資料產生器Test().服務項目(SSID);
+                    ViewBag.評價 = new 資料產生器Test().評價(SSID);
+
+                }
+                return View();
             }
-            return View();
+            else
+            {
+                int SSID = (int)TempData["ID"];
+                var q = db.tDesigner.Where(x => x.fid == SSID).FirstOrDefault();
+                ViewBag.Name = db.tMember.Where(x => x.fID == q.fk_Member).FirstOrDefault().fNickname;
+                if (q != null)
+                {
+
+                    ViewBag.HImg = new 資料產生器Test().取得會員大頭貼(SSID);
+                    ViewBag.Dphoto = new 資料產生器Test().取得設計師作品(SSID);
+                    ViewBag.店鋪資訊 = new 資料產生器Test().設計師店鋪(SSID);
+                    ViewBag.服務項目 = new 資料產生器Test().服務項目(SSID);
+                    ViewBag.評價 = new 資料產生器Test().評價(SSID);
+
+                }
+                return View("", "_LayourMember");
+            }
+            
+        }
+
+        public ActionResult idforD(int id) 
+        {
+            TempData["ID"] = id;
+            return RedirectToAction("Index");
         }
     }
 }
